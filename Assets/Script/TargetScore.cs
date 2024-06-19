b35cc3e7356341e9ebd201a0f277f856254316da
vxGhaa <gantengparts007@gmail.com>
2024-06-19 12:42:37 +0700

    update : Target Score

--- Assets/Script/TargetScore.cs
+++ Assets/Script/TargetScore.cs
@@ -6,16 +6,6 @@ public class TargetScore : MonoBehaviour
 {
     public int PointValue;
 
-    // void Start()
-    // {
-
-    // }
-
-    // private void OnTriggerEnter(Collider other)
-    // {
-    //     if( )
-    // }
-
     public ParticleSystem destroyParticle;
     private void OnCollisionEnter(Collision collision)
     {
